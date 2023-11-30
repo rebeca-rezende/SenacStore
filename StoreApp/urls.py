482afde5581a7bc8cf2497cbadf0bc4f6e22850f
Rebeca Rezende <rebeca.rezende7@gmail.com>
2023-11-29 22:26:29 -0300

    detalhes dos produtos

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -1,8 +1,10 @@
-from django.urls import include, path
+from django.urls import path
 from StoreApp import views
 
-
 urlpatterns = [
-    
     path('', views.index, name='index'),
+    path('produtos/', views.produto_lista, name='produto_lista'),
+    path('produtos/<int:id>', views.produto_lista_por_id, name='produto_lista_por_id'),
+    path('produto/<int:id>', views.produto_detalhe, name='produto_detalhe'),
+
 ]
--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+from StoreApp.models import Departamento, Produto
 
 # Create your views here.
-
 def index(request):
-    return render (request, 'index.html')
+    produtos_em_destaque = Produto.objects.filter(destaque = True)
+    context = {
+        'produtos': produtos_em_destaque
+    }
+    return render(request, 'index.html', context)
+
+def produto_lista(request):
+
+    produtos = Produto.objects.all()
+    context = {
+        'produtos': produtos,
+        'categoria': 'Todos Produtos',
+    }
+    return render(request, 'produtos.html', context)
+
+def produto_detalhe(request, id):
+    produto = Produto.objects.get(id=id)
+    context = {
+        'produto': produto
+    }
+    return render(request, 'produto_detalhes.html', context)
+
+def produto_lista_por_id(request, id):
+    produtos = Produto.objects.filter(departamento_id = id)
+    departamento = Departamento.objects.get(id = id)
+    context = {
+        'produtos': produtos,
+        'categoria': departamento.nome,
+    }
+    return render(request, 'produtos.html', context)
